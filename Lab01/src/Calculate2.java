8d8a4e130690c5c56709f194cce1cbe690be7831
noraainuse <nxx4235@gmail.com>
2025-12-08 00:29:56 +0700

    feat: Lab01 - unfinish

---
+++ Lab01/src/Calculate2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Calculate2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+                int num1, num2 = 6;
+                System.out.print("Enter Value   ");
+                num1 = sc.nextInt();
+                num1 = num1 + 2;
+                num2 = num1 / num2;
+                System.out.println("result = " + num2);
+    }
+}
