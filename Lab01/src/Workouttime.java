25262d9ac7e07f9c991f2b9f3ee74d08d7a05ea5
noraainuse <nxx4235@gmail.com>
2025-12-09 02:24:38 +0700

    feat: Lab01 - all finished

---
+++ Lab01/src/Workouttime.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author lynn
+ */
+public class Workouttime {
+    public static void main(String[] args) {
+        Scanner tip = new Scanner(System.in);
+        System.out.print("Enter your exercise time 1: ");
+        int time1 = tip.nextInt();
+        System.out.print("Enter your exercise time 2: ");
+        int time2 = tip.nextInt();
+        int timeAll = time1 + time2;
+        int sec = timeAll % 60;
+        int min = timeAll / 60;
+        int hour = min / 60;
+        System.out.println("It is " + hour + " hours " + min + " minutes and " + sec + " seconds.");
+    }
+}
